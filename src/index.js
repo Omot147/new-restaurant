6f4b2209f10b85b05b80c4074075eaeb7eaa46fc
Omot147 <omotomoniyi@gmail.com>
2026-01-16 10:54:26 +0100

    created navigation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import "./styles.css";
+import { Home } from './pages/home.js';
 
+const myHome = document.getElementById('btn1');
+const myAbout = document.getElementById('btn2');
+const myService = document.getElementById('btn3');
+const myContact = document.getElementById('btn4');
 
+const container = document.querySelector("#content");
+document.addEventListener("DOMContentLoaded", () => {
+    document.innerHTML = "";
+});
+
+myHome.addEventListener('click', () => {
+    Home.reset();
+    Home.loadHome();
+});
+
+myAbout.addEventListener('click', () => {
+    Home.reset();
+    Home.loadAbout();
+});
 console.log("Hello, welcome to My Restaurant!");
